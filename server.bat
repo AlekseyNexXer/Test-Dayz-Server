3ab9d58c1cd632685852c8310440d98219d58c1e
Aleks Ignatov <arkeyinquisitor@gmail.com>
2021-11-26 18:38:17 +0300

    - Add BEC

---
+++ server.bat
@@ -0,0 +1,3 @@
+Start bec.bat
+"arma2oaserver.exe" -port=2302 "-config=instance_24_Napf\config.cfg" "-cfg=instance_24_Napf\basic.cfg" "-profiles=instance_24_Napf" -name=instance_24_Napf "-mod=@DayZ_Epoch;@DayZ_Epoch_Server;"
+exit
