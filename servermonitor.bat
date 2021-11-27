e5cbc79deaf77c75b6db74a7ff990daeb0e868e7
Aleks <arkeyinquisitor@gmail.com>
2021-11-27 18:31:51 +0300

    MassMod ребаланс

--- servermonitor.bat
+++ servermonitor.bat
@@ -1,12 +1,12 @@
 @echo off
-title EpochServer Monitor
+title DayzServer Monitor
 
 :start
 C:\Windows\System32\tasklist /FI "IMAGENAME eq DayZServer_x64.exe" 2>NUL | C:\Windows\System32\find /I /N "DayZServer_x64.exe">NUL
 if "%ERRORLEVEL%"=="0" goto loop
 cls
 echo Server is not running, restarting... 
-start "Restarting Server" /wait "D:\DayzServer\start_server.bat"
+start "Restarting Server" /wait "D:\DayzServer\startserver.bat"
 echo Server started succesfully, switching to monitor loop
 timeout 30
 cls
